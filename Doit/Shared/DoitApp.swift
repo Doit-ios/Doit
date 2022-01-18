ae6a250f152deb3b2b7e37d7ca7ddbf57c9aabf6
hanni66 <haeunkim0807@naver.com>
2022-01-18 10:06:59 +0900

    Feat: start

---
+++ Doit/Shared/DoitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoitApp.swift
+//  Shared
+//
+//  Created by 김하은 on 2022/01/18.
+//
+
+import SwiftUI
+
+@main
+struct DoitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
